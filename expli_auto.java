3b4b1a3dc4d2f8fbd834c345011e427c5c40764e
amith0608 <85832517+amith0608@users.noreply.github.com>
2021-06-15 18:43:21 +0530

    Add files via upload

---
+++ expli_auto.java
@@ -0,0 +1,15 @@
+package type_c.collections;
+
+public class expli_auto {
+
+	public static void main(String[] args) {
+		
+		int myInt = 9;
+		double myDouble = myInt;
+		System.out.println(myInt);
+		System.out.println(myDouble);
+
+
+	}
+
+}
---
+++ impli.java
@@ -0,0 +1,14 @@
+package type_c.collections;
+
+public class impli {
+
+	public static void main(String[] args) {
+		double myDouble = 9.78;
+	    int myInt = (int) myDouble; // Manual casting: double to int
+
+	    System.out.println(myDouble);   
+	    System.out.println(myInt);      
+
+	}
+
+}
